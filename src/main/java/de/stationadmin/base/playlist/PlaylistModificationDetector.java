792633138b39c5681ae9e06ac8b8cf9891318ea4
fkorf <fkorf@LAPTOP-86F1JBIB.fritz.box>
2020-10-27 18:17:04 +0100

    Fixed issues in playlist modifiation detector

--- src/main/java/de/stationadmin/base/playlist/PlaylistModificationDetector.java
+++ src/main/java/de/stationadmin/base/playlist/PlaylistModificationDetector.java
@@ -21,98 +21,103 @@ import de.stationadmin.lfm.backend.PlaylistHead;
  * 
  */
 public class PlaylistModificationDetector extends AbstractBean {
-  private static final Logger log = Logger.getLogger(PlaylistModificationDetector.class);
-  private SessionCtx ctx;
-  private PlaylistRegistry playlistRegistry;
-  private boolean modified = false;
-  private int[] modifiedPlaylistIds;
+	private static final Logger log = Logger.getLogger(PlaylistModificationDetector.class);
+	private SessionCtx ctx;
+	private PlaylistRegistry playlistRegistry;
+	private boolean modified = false;
+	private int[] modifiedPlaylistIds;
 
-  public PlaylistModificationDetector(SessionCtx ctx, PlaylistRegistry playlistRegistry) {
-    super();
-    this.ctx = ctx;
-    this.playlistRegistry = playlistRegistry;
-  }
+	public PlaylistModificationDetector(SessionCtx ctx, PlaylistRegistry playlistRegistry) {
+		super();
+		this.ctx = ctx;
+		this.playlistRegistry = playlistRegistry;
+	}
 
-  public int[] detectModifiedPlaylists() {
-    try {
-      Set<Integer> ids = new HashSet<Integer>();
-      Set<Integer> modifiedIds = new HashSet<Integer>();
-      for (Playlist playlist : this.playlistRegistry.getPlaylists(PlaylistType.ONLINE)) {
-        ids.add(playlist.getId());
-      }
-      for (PlaylistHead head : ctx.getServer().getPlaylists(ctx.getStationId())) {
-        Playlist playlist = this.playlistRegistry.getPlaylist(head.getId());
-        if (playlist == null || playlist.getUpdatedAt() == null || head.getUpdatedAt().getTime() > playlist.getUpdatedAt().getTime()) {
-          modifiedIds.add(head.getId());
-          log.info("Playlist " + head.getTitle() + " detected as modified - updatedAt changed (" + playlist.getUpdatedAt() + " / " + head.getUpdatedAt() + ")");
-        } else if (playlist.isShuffle() && !playlist.isShuffleTypeSet()) {
-          modifiedIds.add(head.getId());
-          log.info("Playlist " + head.getTitle() + " detected as modified - shuffle type unknown");
-        }
-        ids.remove(head.getId());
-      }
-      if (ids.size() > 0) {
-        // playlists have been deleted
-        log.info("Playlists " + ids + " have been deleted");
-        modifiedIds.addAll(ids);
-      }
+	public int[] detectModifiedPlaylists() {
+		try {
+			Set<Integer> ids = new HashSet<Integer>();
+			Set<Integer> modifiedIds = new HashSet<Integer>();
+			for (Playlist playlist : this.playlistRegistry.getPlaylists(PlaylistType.ONLINE)) {
+				ids.add(playlist.getId());
+			}
+			for (PlaylistHead head : ctx.getServer().getPlaylists(ctx.getStationId())) {
+				Playlist playlist = this.playlistRegistry.getPlaylist(head.getId());
+				if (playlist == null || playlist.getUpdatedAt() == null
+						|| head.getUpdatedAt().getTime() > playlist.getUpdatedAt().getTime()) {
+					modifiedIds.add(head.getId());
+					log.info("Playlist " + head.getTitle() + " detected as modified - updatedAt changed");
+				} else if (playlist.isShuffle() && !playlist.isShuffleTypeSet()) {
+					modifiedIds.add(head.getId());
+					log.info("Playlist " + head.getTitle() + " detected as modified - shuffle type unknown");
+				}
+				ids.remove(head.getId());
+			}
+			if (ids.size() > 0) {
+				// playlists have been deleted
+				log.info("Playlists " + ids + " have been deleted");
+				modifiedIds.addAll(ids);
+			}
 
-      if (modifiedIds.size() > 0) {
-        int[] modifiedPlaylistIds = new int[modifiedIds.size()];
-        int idx = 0;
-        for (Integer id : modifiedIds) {
-          modifiedPlaylistIds[idx++] = id;
-        }
-        return modifiedPlaylistIds;
-      }
+			if (modifiedIds.size() > 0) {
+				int[] modifiedPlaylistIds = new int[modifiedIds.size()];
+				int idx = 0;
+				for (Integer id : modifiedIds) {
+					modifiedPlaylistIds[idx++] = id;
+				}
+				return modifiedPlaylistIds;
+			}
 
-    } catch (IOException e) {
-      log.error("unable to check for modified playlists");
-    }
+		} catch (IOException e) {
+			log.error("unable to check for modified playlists");
+		}
 
-    return null;
-  }
+		return null;
+	}
 
-  public void check() {
-    this.modifiedPlaylistIds = this.detectModifiedPlaylists();
-    setModified(this.modifiedPlaylistIds != null && this.modifiedPlaylistIds.length > 0);
-  }
+	public void check() {
+		this.modifiedPlaylistIds = this.detectModifiedPlaylists();
+		setModified(this.modifiedPlaylistIds != null && this.modifiedPlaylistIds.length > 0);
+	}
 
-  public TimerTask getCheckTask() {
+	public TimerTask getCheckTask() {
 
-    return new TimerTask() {
-      @Override
-      public void run() {
-        check();
-      }
+		return new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					check();
+				} catch (Exception e) {
+					log.error("playlist modification detection failed, e");
+				}
+			}
 
-    };
+		};
 
-  }
+	}
 
-  /**
-   * @return the modified
-   */
-  public boolean isModified() {
-    return modified;
-  }
+	/**
+	 * @return the modified
+	 */
+	public boolean isModified() {
+		return modified;
+	}
 
-  /**
-   * @param modified the modified to set
-   */
-  public void setModified(boolean modified) {
-    boolean old = this.modified;
-    this.modified = modified;
-    this.firePropertyChange("modified", old, modified);
-  }
+	/**
+	 * @param modified the modified to set
+	 */
+	public void setModified(boolean modified) {
+		boolean old = this.modified;
+		this.modified = modified;
+		this.firePropertyChange("modified", old, modified);
+	}
 
-  public void markClean() {
-    setModified(false);
-    this.modifiedPlaylistIds = null;
-  }
+	public void markClean() {
+		setModified(false);
+		this.modifiedPlaylistIds = null;
+	}
 
-  public int[] getModifiedPlaylistIds() {
-    return modifiedPlaylistIds;
-  }
+	public int[] getModifiedPlaylistIds() {
+		return modifiedPlaylistIds;
+	}
 
 }
