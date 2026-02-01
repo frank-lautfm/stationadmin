dc3610dcd58cb165e2439403b0aa504a17513800
Frank Korf <fkorf@frank-korf.de>
2026-02-01 10:28:12 +0100

    Updated version to 5.5.1

--- src/main/java/de/stationadmin/base/Version.java
+++ src/main/java/de/stationadmin/base/Version.java
@@ -12,7 +12,7 @@ public interface Version {
 
   int MAJOR = 5;
   int MINOR = 5;
-  int STEP = 0;
+  int STEP = 1;
   String STATUS = "";
 
   String VERSION = MAJOR + "." + MINOR + "." + STEP + " " + STATUS;
